ba57a60fc59b27993abbbc2bdc08d7f1e50ceea9
Ace Wood <acewood@AcedeMacBook-Pro.local>
2021-07-11 18:05:19 +0800

    $1

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,5 @@
 import layoutHeaderAside from '@/layout/header-aside'
 import mlefLayout from '@/layout/mlef-layout'
-import productsLayout from '@/layout/productsLayout'
 // 由于懒加载页面太多的话会造成webpack热更新太慢，所以开发环境不使用懒加载，只有生产环境使用懒加载
 const _import = require('@/libs/util.import.' + process.env.NODE_ENV)
 
